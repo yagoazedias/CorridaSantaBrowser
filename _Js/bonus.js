b91f6ee561c6a73ecee10667b475c1eefd9023a5
yagoazedias <yagoluis9@hotmail.com>
2015-06-23 10:37:41 -0300

    Version 1.1

--- _Js/bonus.js
+++ _Js/bonus.js
@@ -1,9 +1,9 @@
 var bonus = function()
 {
 	this.image = new Image();
-	this.image.src = "./_Images/\Bonus/BonusGold.png";
+	this.image.src = "./_Images/Bonus/BonusGold.png";
 	
-	this.radomImage = ["./_Images/\Bonus/BonusRed.png","./_Images/\Bonus/BonusBlue.png","./_Images/\Bonus/BonusGold.png"]
+	this.radomImage = ["./_Images/Bonus/BonusRed.png","./_Images/Bonus/BonusBlue.png","./_Images/Bonus/BonusGold.png"]
 	
 	this.x = 3000;
 	this.y = 200 + Math.floor(Math.random() * (graphics.canvas.height - 300));
@@ -13,6 +13,12 @@ var bonus = function()
 	this.update = function()
 	{
 		this.x -= 5;
+		
+		if(this.x < 0 - this.w)
+		{ this.x = 3000; this.image.src = bonus.radomImage[Math.floor(Math.random() * (bonus.radomImage.length))];}
+		
+		if(this.x < 2000)
+		{ player.effect = "Normal"; }
 	}
 	
 	this.draw = function()
--- _Js/colision.js
+++ _Js/colision.js
@@ -16,10 +16,18 @@ var Colision = function()
 			if((player.x > arrow3.x - player.w + 50)&&(player.x < arrow3.x)&&((player.y < arrow3.y - 10)&&(player.y > arrow3.y - player.h + 30)))
 			{arrow3.x = 1000; arrow3.y = 200 + Math.floor(Math.random() * (graphics.canvas.height - 300)); graphics.ctx.drawImage(this.hit,0,0);} 
 			
+			//colisão com o player bonus
 			if((player.x > bonus.x - player.w + 50)&&(player.x < bonus.x)&&((player.y < bonus.y - 10)&&(player.y > bonus.y - player.h + 30)))
-			{bonus.x = 3000; bonus.y = 200 + Math.floor(Math.random() * (graphics.canvas.height - 300)); bonus.image.src = bonus.radomImage[Math.floor(Math.random() * (bonus.radomImage.length))];} 
-			
-			console.log(bonus.radomImage.length);
+			{
+				bonus.x = 3000;
+				bonus.y = 200 + Math.floor(Math.random() * (graphics.canvas.height - 300)); 
+				bonus.image.src = bonus.radomImage[Math.floor(Math.random() * (bonus.radomImage.length))];
+					
+				if(bonus.image.src === "./_Images/Bonus/BonusRed.png")
+				{
+					player.effect = "Fast";
+				}
+			} 
 		}
 	}
 }
