6924b11a37f42626e11b439bee30a0847dec1e5d
yagoazedias <yagoluis9@hotmail.com>
2015-10-23 10:12:47 -0200

    Update keyboard.js

--- _Js/keyboard.js
+++ _Js/keyboard.js
@@ -1,48 +1,28 @@
-var keyboard = function()
+var keyboard = (function()
 {
-	this.keydown = function(e)
+	this.keydown = (function(e)
 	{
 		switch(e.keyCode)
 		{
-			case 39:
-				player.moveRight = true;
-				break;
-			case 37:
-				player.moveLeft = true;
-				break;
-			case 38:
-				player.moveUp = true;
-				break;
-			case 40:
-				player.moveDown = true;
-				break;
-			case 80:
-				if(screenGame.GameScene === "Scene_Game")
-				{gameplay.pause = !gameplay.pause;}
-				break;
+			case 39: player.moveRight = true; break;
+			case 37: player.moveLeft = true; break;
+			case 38: player.moveUp = true; break;
+			case 40: player.moveDown = true; break;
+			case 80: if(screenGame.GameScene === "Scene_Game") {gameplay.pause = !gameplay.pause;} break;
 		}
-	}
+	});
 	
-	this.keyup = function(e)
-	{
+	this.keyup = (function(e) {
+	
 		switch(e.keyCode)
 		{
-			case 39:
-				player.moveRight = false;
-				break;
-			case 37:
-				player.moveLeft = false;
-				break;
-			case 38:
-				player.moveUp = false;
-				break;
-			case 40:
-				player.moveDown = false;
-				break;
+			case 39: player.moveRight = false; break;
+			case 37: player.moveLeft = false; break;
+			case 38: player.moveUp = false; break;
+			case 40: player.moveDown = false; break;
 		}
-	}
-}
-
+	});
+});
 var keyboard = new keyboard();
 
 document.addEventListener('keydown', keyboard.keydown, true);
