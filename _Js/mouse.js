90f05ce93403fef45033e3be12e43d6bd4d4704f
yagoazedias <yagoluis9@hotmail.com>
2015-10-23 10:15:04 -0200

    Update mouse.js

--- _Js/mouse.js
+++ _Js/mouse.js
@@ -1,11 +1,10 @@
-var Mouse = function()
-{
+var Mouse = (function() {
+
 	this.x = 0;
 	this.y = 0;
 	this.click = false;
 	
-	this.update = function()
-	{
+	this.update = function(){
 		//console.log("MouseX: " + mouse.x + " MouseY: " + mouse.y);
 	}
 	
@@ -13,5 +12,5 @@ var Mouse = function()
 	{
 		
 	}
-}
+});
 var mouse = new Mouse();
