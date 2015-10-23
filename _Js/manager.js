85a0425bd19a109e098d58430974a46c497a1970
yagoazedias <yagoluis9@hotmail.com>
2015-10-23 10:13:47 -0200

    Update manager.js

--- _Js/manager.js
+++ _Js/manager.js
@@ -1,20 +1,18 @@
-var Manager = function()
+var Manager = (function()
 {
-	this.update = function()
-	{
+	this.update = (function() {
 		fadeIn.update();
 		screenGame.update();
 		gameplay.update();
 		mouse.update();
-	}
+	});
 	
-	this.draw = function()
-	{
+	this.draw = (function() {
 		graphics.clear();
 		screenGame.draw();
 		gameplay.draw();
 		fadeIn.draw();
-	}
-}
+	});
+});
 
 var manager = new Manager();
