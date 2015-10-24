9397513425bd2edc0aa0b89e96265a9227fe7870
yagoazedias <yagoluis9@hotmail.com>
2015-10-24 12:23:02 -0200

    Update score.js

--- _Js/score.js
+++ _Js/score.js
@@ -1,5 +1,4 @@
-var score = function()
-{
+var score = (function() {
 	this.number = 0;
 	this.count = 0.25;
 	
@@ -8,5 +7,5 @@ var score = function()
 		if(screenGame.GameScene === "Scene_Game" && !gameplay.pause && player.life > 0)
 		{this.number += this.count;}
 	}	
-}
+});
 var score = new score();
