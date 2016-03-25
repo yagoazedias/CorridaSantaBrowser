b861ea4ce79d8674cd3c734b2f09d9fc1d81a3fa
yagoazedias <yagoluis9@hotmail.com>
2016-03-25 19:09:26 -0300

    Version 0.0 - reboot

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var Main = (function()
+{
+    // This is the main update, here all game will be update.
+    this.update = (function()
+    {
+            
+    });
+    
+    // This is the main draw, here all game will be draw.
+    this.draw = (function()
+    {
+            
+    });
+});
+var main = new Main();
