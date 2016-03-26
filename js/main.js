557ad2c863ab9341461ea211fc23779ca76cc007
yagoazedias <yagoluis9@hotmail.com>
2016-03-26 12:27:11 -0300

    Version 0.1

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ var Main = (function()
     // This is the main update, here all game will be update.
     this.update = (function()
     {
-            
+        map.update();
     });
     
     // This is the main draw, here all game will be draw.
     this.draw = (function()
     {
-            
+        canvas.clear();
+        map.draw();
     });
 });
 var main = new Main();
