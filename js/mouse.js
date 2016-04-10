610a3304dc574e49297fa230485bee168f5d9a42
yagoazedias <yagoazedias@gmail.com>
2016-04-10 18:44:54 -0300

    Update mouse.js

--- js/mouse.js
+++ js/mouse.js
@@ -1,16 +1,20 @@
 var Mouse = (function()
 {
+	// Position propietes.
 	this.x = 0;
 	this.y = 0;
 	
+	// Click propiete.
 	this.click = false;
 	
+	// Mouse move function detect.
 	var _mouseMove = (function(e)
 	{
 		mouse.x = e.x - canvas.element.offsetLeft;
 		mouse.y = e.y - canvas.element.offsetTop;
 	});
-
+	
+	// Mouse down function detect.
 	var _mouseDown = (function(e)
 	{
 		mouse.click = true;
@@ -18,25 +22,17 @@ var Mouse = (function()
 		mouse.y = e.y - canvas.element.offsetTop;
 	});
 	
+	// Mouse up function detect.
 	var _mouseUp = (function(e)
 	{
 		mouse.click = false;
 		mouse.x = e.x - canvas.element.offsetLeft;;
 		mouse.y = e.y - canvas.element.offsetTop;
 	});
-
+	
+	// Linking the functions to your eventlistener.
 	document.addEventListener("mousemove", _mouseMove, false);
 	document.addEventListener("mousedown", _mouseDown, false);
 	document.addEventListener("mouseup",   _mouseUp,   false);
-	
-	this.update = (function()
-	{
-		console.log("graphics.OffSetX: " + this.x + " graphics.OffSetY: " + this.y);
-	});
-	
-	this.draw = (function()
-	{
-		graphics.drawRect(this.x,this.y,10,10,"red");
-	});
 });
 var mouse = new Mouse();
